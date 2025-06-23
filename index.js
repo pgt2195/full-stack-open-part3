0eca6657745563a2bf4e7954ce1665bad08dc155
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-06-23 17:09:05 +0200

    Terminé les exercices 3.7 et 3.8

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
+
+morgan.token('postBody', (req, res) => JSON.stringify(req.body))
 
 let persons = [
     { 
