45233e44a618a40d151f92bfad6531b84bc77618
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-06-24 21:33:50 +0200

    uibuild 2025-06-24 21:33:50

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
 morgan.token('postBody', (req, res) => JSON.stringify(req.body))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
